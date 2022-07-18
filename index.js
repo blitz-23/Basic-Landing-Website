fdd94d35fc322fe184da3140d465734e6ad14a92
blitz-23 <94664667+blitz-23@users.noreply.github.com>
2022-07-18 11:19:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let person={
+    name: 'Abhinab',
+    age: 22
+};
+window.alert('Hello');
+console.log(person.name);
+console.log(person.age);
+name=prompt('Please enter your Name:');
+document.write("Bye "+name+"!!!😀😁👋");
